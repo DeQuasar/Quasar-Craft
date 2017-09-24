47047ade547b7dcf80d40f099e0d7129431e726e
DeQuasar <tonypro999@gmail.com>
2017-09-23 21:56:46 -0400

    Added PHPDoc blocks

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,11 +2,28 @@
 
 namespace Api\Controllers;
 
-use Psr\Container\ContainerInterface;
+use \Psr\Container\ContainerInterface;
+
+/**
+ * Class BaseController
+ * @package Api\Controllers
+ */
 
 class BaseController
 {
+    /**
+     * @var \Psr\Container\ContainerInterface $container
+     */
+
     protected $container;
+
+    /**
+     * BaseController constructor.
+     *
+     * Instantiates the container for children controllers.
+     *
+     * @param \Psr\Container\ContainerInterface $container
+     */
 
     public function __construct(ContainerInterface $container)
     {
